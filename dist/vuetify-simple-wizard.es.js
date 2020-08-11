d77c06f0962bf8ad35bd3d2f77031697f3e2c77d
Jason Wilczak <jason.wilczak@gmail.com>
2020-08-11 18:45:07 -0400

    adding bundled 2.1 code to dist

--- dist/vuetify-simple-wizard.es.js
+++ dist/vuetify-simple-wizard.es.js
@@ -1,5 +1,5 @@
 /**
- * vuetify-simple-table v2.0.0
+ * vuetify-simple-table v2.1.0
  * https://github.com/jasonwilczak/vuetify-simple-wizard
  * Released under the MIT License.
  */
@@ -59,6 +59,7 @@ var SimpleWizard = { render: function () {
         return false;
       }
     },
+    scrollToTopOnStepChange: { default: true },
     mobileBreakpoint: { default: 960 },
     theme: {
       type: Object,
@@ -98,6 +99,7 @@ var SimpleWizard = { render: function () {
   watch: {
     stepStage: function () {
       this.currentStep = this.stepStage - 1;
+      if (this.scrollToTopOnStepChange === true) { this.goToTopOnNext(); }
     },
     currentStep: function () {
       this.currentStepOptions = this.steps[this.currentStep].options || {};
@@ -169,6 +171,11 @@ var SimpleWizard = { render: function () {
       if (this.developer.verboseLogging) {
         if (this.developer.logger && typeof this.developer.logger == 'function') { this.developer.logger(message); }else { console.log(message); }
       }
+    },
+    // When the user clicks on the button, scroll to the top of the document
+    goToTopOnNext: function goToTopOnNext() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }
 
   }
